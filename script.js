f9d4686bc9e39328d4d0079d429ec993348ff7b0
AminAnv1 <aminanvari77@gmail.com>
2024-10-22 08:00:01 +0330

    color improved

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-const startTime = new Date('2024-10-21T23:54:00');
-const finishTime = new Date('2024-10-21T23:54:20');
+const startTime = new Date('2024-10-22T07:54:00');
+const finishTime = new Date('2024-10-22T07:54:20');
 
 const hoursBox = document.getElementById('hours');
 const minutesBox = document.getElementById('minutes');
 const secondsBox = document.getElementById('seconds');
+const timeBoxes = document.querySelectorAll('.time-box');
 
 const message = document.getElementById('message');
 
 message.textContent = "";
 
 countdownInterval = setInterval(() => {
-    var now = new Date();
-    var show = 0;
+    const now = new Date();
+    let show = 0;
 
     if (now < startTime) {
-        message.textContent = "مسابق هنوز شروع نشده است.";
+        message.textContent = "مسابقه هنوز شروع نشده است.";
         show = finishTime - startTime;
-     
-    }
+    } 
     else if (now > finishTime) {
         message.textContent = "مسابقه به پایان رسیده است.";
         show = 0;
-    }
+    } 
     else {
         message.textContent = "مسابقه در حال برگزاری است.";
         show = finishTime - now;
